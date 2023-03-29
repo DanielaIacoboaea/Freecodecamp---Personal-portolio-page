8a8a22a4fca36d662caecf5ed114084dbfe160ca
Daniela <iacoboaea@gmail.com>
2023-03-29 15:28:45 +0100

    Fix change theme buttons

--- change-theme.js
+++ change-theme.js
@@ -1,14 +1,28 @@
 {
 let darkThemeButton = document.querySelector("#dark-theme-button");
 let lightThemeButton = document.querySelector("#light-theme-button");
-            
+
 darkThemeButton.addEventListener("click", _ => {
-    document.body.classList.remove("light-theme");
-    document.body.classList.add("dark-theme");
+
+    if (document.body.classList.contains("dark-theme")){
+        document.body.classList.remove("dark-theme");
+        document.body.classList.add("light-theme");
+        lightThemeButton.checked = true;
+    }else{
+        document.body.classList.remove("light-theme");
+        document.body.classList.add("dark-theme");
+    }
 });
 
 lightThemeButton.addEventListener("click", _ => {
-    document.body.classList.remove("dark-theme");
-    document.body.classList.add("light-theme");
+
+    if (document.body.classList.contains("light-theme")){
+        document.body.classList.remove("light-theme");
+        document.body.classList.add("dark-theme");
+        lightThemeButton.checked = false;
+    }else{
+        document.body.classList.remove("dark-theme");
+        document.body.classList.add("light-theme");
+    }
 });
 }
